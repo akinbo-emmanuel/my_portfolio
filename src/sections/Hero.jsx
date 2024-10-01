dcada48e37137d3f467ee83d073e6f5113650893
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-10-01 17:21:48 +0100

    Favicon updated and Hero section setup

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return (
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p>Hi, I am Emmanuel <span>👋</span></p>
+            </div>
+        </section>
+    )
+}
+export default Hero
